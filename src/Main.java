42e56693273a16b08b24561f90a42e9fcad71a89
HeriniavotraToky <heriniavotratoky@gmail.com>
2025-07-09 16:01:50 +0300

    creation des methodes dans la classe fifo

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
-
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         int port = 8081;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
         server.createContext("/", exchange -> {
-            String response = "Bienvenue sur l'API Java !";
-            exchange.getResponseHeaders().set("Content-Type", "text/plain");
-            exchange.sendResponseHeaders(200, response.length());
-            exchange.getResponseBody().write(response.getBytes());
-            exchange.close();
+            String msg = "✅ Bienvenue sur l'API FIFO Java";
+            sendResponse(exchange, 200, msg);
         });
+      
+        server.createContext("/tickets", new TaskHandler());
 
-        server.setExecutor(null); 
+        server.setExecutor(null);
         System.out.println("✅ Serveur HTTP démarré sur le port " + port);
         server.start();
+    }
+
+    private static void sendResponse(com.sun.net.httpserver.HttpExchange exchange, int statusCode, String response)
+            throws IOException {
+        exchange.sendResponseHeaders(statusCode, response.getBytes().length);
+        try (java.io.OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
     }
 }
